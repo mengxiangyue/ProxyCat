ddec86868bc226ad49a0df575570625255b56277
mengxiangyue <598660766@qq.com>
2023-02-07 22:43:37 +0800

    clean code

--- Sources/ProxyCat/ProxyCat.swift
+++ Sources/ProxyCat/ProxyCat.swift
@@ -7,8 +7,6 @@ public struct ProxyCat {
     public private(set) var text = "Hello, World!"
 
     public static func main() {
-        let url = URL(string: "http://www.gov.cn")
-        print(url?.host)
         Task {
             let proxyServer = ProxyServer()
             await proxyServer.start()
