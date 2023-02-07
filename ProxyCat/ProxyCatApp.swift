dbd2c5897133d923ccbbe64f9d9b24a330063d1d
mengxiangyue <598660766@qq.com>
2023-02-07 23:19:18 +0800

    update file folder

---
+++ ProxyCat/ProxyCatApp.swift
@@ -0,0 +1,18 @@
+//
+//  ProxyCatApp.swift
+//  ProxyCat
+//
+//  Created by xiangyue on 2023/2/7.
+//
+
+import SwiftUI
+import ProxyCatCore
+
+@main
+struct ProxyCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
