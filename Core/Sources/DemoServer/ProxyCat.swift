2dd9e2282f7997ddde912a85801cac9f7b552f7f
mengxiangyue <598660766@qq.com>
2023-07-30 19:04:46 +0800

    add the comments and fix compile issue

--- Core/Sources/DemoServer/ProxyCat.swift
+++ Core/Sources/DemoServer/ProxyCat.swift
@@ -2,6 +2,11 @@ import ProxyCatCore
 import Foundation
 import Dispatch
 
+/// test https curl -v -x http://localhost:8080  https://www.apple.com
+/// test https curl -v -x http://localhost:8080 -k  https://www.baidu.com
+/// -k ignore certification check
+/// test http curl -v -x http://localhost:8080  http://www.maitanbang.com/test/
+/// right now http proxy does not handle the 301
 @main
 public struct ProxyCat {
     public private(set) var text = "Hello, World!"
--- Core/Tests/DemoServerTests/DemoServerTests.swift
+++ Core/Tests/DemoServerTests/DemoServerTests.swift
@@ -1,5 +1,5 @@
 import XCTest
-@testable import ProxyCat
+@testable import ProxyCatCore
 
 final class ProxyCatTests: XCTestCase {
     func testExample() throws {
